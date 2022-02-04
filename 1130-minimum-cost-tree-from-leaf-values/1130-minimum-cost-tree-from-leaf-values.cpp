ee982269867e851a041f0845f2103241cba8f715
Wei wei <83396897+Wadiya003@users.noreply.github.com>
2022-02-05 00:11:33 +0530

    Time: 2 ms (83.56%), Space: 8.4 MB (42.41%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,36 +1,26 @@
 class Solution {
-public: //IDEA->DP(MEMOIZATION)
-//     1)l=0;h=n-1;
-//     make left subtree from  l to i ;
-//     make right subtree from i+1 to h;
-//     take max ;
-    
+public:
     int mctFromLeafValues(vector<int>& arr) {
-        int  h =arr.size();
-        vector<vector<int>> dp(h+1, vector<int>(h+1,-1));
-        return helper(0,h-1,arr, dp);
-    }
-    int helper (int l , int h , vector<int>&arr , vector<vector<int>> &dp){
-       //BASE CASES
-        if(l==h)return dp[l][h]=0;
-        if(h-l==1)return dp[l][h]=arr[h]*arr[l];
-        if (dp[l][h]!=-1)return dp[l][h];
-        int ans=INT_MAX;
-        for(int i =l;i<h ;i++){
-            int left =highest(l,i,arr);
-            int right=highest(i+1,h,arr);
-            int add =helper(l,i,arr,dp)+helper(i+1,h,arr,dp);
-            ans =min(ans,(left*right)+add);
+        int n=arr.size();
+        int sum=0;
+        int pro;
+        stack <int> st;
+        for(auto a: arr){
+        while(!st.empty() && a>st.top()){
+            pro=st.top();
+            st.pop();
+            if(st.empty()) sum+=pro*a;
+            else sum+=pro*(min(st.top(),a));
+        }st.push(a);
         }
-        return dp[l][h]=ans;
+        while(!st.empty()){
+            pro=st.top();
+            st.pop();
+            if (!st.empty()) sum += st.top()*pro;
+       }
+        
+        return sum;
     }
-    int highest(int l , int h , vector<int>&arr){
-        int maxelement =0;
-        for(int i =l;i<= h;i++){
-        maxelement=max(maxelement, arr[i]);
-        }
-        return maxelement ;
-    }
-    
- 
 };
+ 
+       
