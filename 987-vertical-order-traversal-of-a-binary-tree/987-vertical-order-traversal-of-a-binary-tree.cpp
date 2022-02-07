9c22aedd001b70a7bb45310f959840d5b082ee51
Wei wei <83396897+Wadiya003@users.noreply.github.com>
2022-02-08 00:28:13 +0530

    Time: 10 ms (27.69%), Space: 12 MB (82.13%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,52 +12,48 @@
 class Solution {//bfs and map pair
 public://keep track of horizontal distances
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-           vector<vector<int>>ans; 
-        if(root == NULL)
-        return ans;
+        vector<vector<int>> ans;
+        map<int,vector<pair<int,int>>>m;
         
-        queue<pair<TreeNode*,int>>qu;//val and col
-        qu.push({root,0});
+        if(!root)return ans;
+        queue<pair<TreeNode*,int>>q;
         int row=0;
-        map<int,vector<pair<int,int>>> m; //col,val and row
-		
-		//bfs traversal
-        while(!qu.empty())
-        {
-            int size = qu.size();
-            while(size--){
-                TreeNode* temp = qu.front().first;
-                int col = qu.front().second;
-                qu.pop();
+        q.push({root,0});
+        while(!q.empty()){
+            int n =q.size();
+            while(n--){
+                TreeNode* temp =q.front().first;
+                int col=q.front().second;
+                q.pop();
                 m[col].push_back({temp->val,row});
-                if(temp->left)
-                qu.push({temp->left,col-1});
-                if(temp->right)
-                qu.push({temp->right,col+1});
-            }
-            row++; 
+                if(temp->left){
+                q.push({temp->left,col-1});
+                }
+                if(temp->right){
+                 q.push({temp->right,col+1});   
+                }
+            }row++;
         }
-        
-		
-		//putting values in vector
         vector<pair<int,int>> v;
         for(auto it=m.begin();it!=m.end();it++){
-            v=it->second;//val and row
+            v=it->second;
             sort(v.begin(),v.end(),[](pair<int,int> a,pair<int,int> b)
             {
-               if(a.second==b.second)  //if same row then take less val
+               if(a.second==b.second)
                    return a.first<b.first;
                else 
-                   return a.second<b.second;//else smaller row 
+                   return a.second<b.second;
             });
             vector<int> v1;
             for(auto i:v){
-                v1.push_back(i.first); 
+                v1.push_back(i.first);
 
             }
             ans.push_back(v1);
             v1.clear();  
         }
         return ans;
+
+        
     }
 };
