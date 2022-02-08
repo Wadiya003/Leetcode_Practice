eabb6799cadd57fe504e51a0082d7278c152650e
Wei wei <83396897+Wadiya003@users.noreply.github.com>
2022-02-09 00:00:28 +0530

    Time: 90 ms (13.42%), Space: 18.5 MB (25.38%) - LeetHub

--- 143-reorder-list/143-reorder-list.cpp
+++ 143-reorder-list/143-reorder-list.cpp
@@ -9,37 +9,29 @@
  * };
  */
 class Solution {
-public:
-    ListNode* middle(ListNode* head) {
-        auto slow = head, fast = head;
-        while(fast && fast -> next)
-            slow = slow -> next,
-            fast = fast -> next -> next;           
-        return slow;                               
-    }
-	
-    ListNode* reverseList(ListNode* head) {
-        ListNode *prev = nullptr;
-        while(head) {
-            auto nextNode = head -> next;         
-            head -> next = prev;                    
-            prev = head;                            
-            head = nextNode;                       
-        }
-        return prev;                              
-    }
+public://using stacks
     void reorderList(ListNode* head) {
-        if(!head || !head -> next) return;
-       auto mid = middle(head);
-       auto R = reverseList(mid), L = head -> next;
-        for(int i = 0; L != R; i++, head = head -> next)    
-            if(i & 1) {                              
-                head -> next = L;
-                L = L -> next;
-            }
-            else {
-                head -> next = R;
-                R = R -> next;
-            }
+        if(!head || !head->next || !head->next->next)return;
+        ListNode* temp=head;
+        stack<ListNode*> st;
+        //push all nodes in stack 
+        while(temp){
+         st.push(temp);
+        temp=temp->next;
+        }
+        int size=st.size();
+        ListNode* tempo=head;
+        //connect
+        for(int i =0;i< size/2;i++){
+        ListNode* last=st.top();
+        st.pop();
+        last->next=tempo->next;
+        tempo->next=last;
+        tempo=tempo->next->next;
+        }
+          
+        tempo->next=NULL;
+        return;
+        
     }
 };
