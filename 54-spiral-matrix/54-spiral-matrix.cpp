4e8544d6a51aa9f1827e8f2db9198041f78121b4
Wei wei <83396897+Wadiya003@users.noreply.github.com>
2022-02-17 00:33:53 +0530

    Time: 4 ms (17.81%), Space: 7 MB (30.23%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int row=matrix.size();
-    int col=matrix[0].size();
-        vector<int>v(row*col);
-int rowstart=0;//to iterate form the starting of rows
-int rowend=row-1; // to iterate from the last rows
-int colstart=0; // to iterate from the starting of columns
-int colend=col-1; // to iterate from the last columns
-//helix matrix
-int p=0;
-while (rowstart<=rowend &&colstart<=colend){
-    
-for(int col=colstart;col<=colend;col++){
-v[p++]=matrix[rowstart][col];
-}if(++rowstart>rowend){break;}
-
-for(int row=rowstart;row<=rowend;row++){
-v[p++]=matrix[row][colend];
-}if(--colend<colstart){break;}
-
-for (int col=colend;col>=colstart;col--){
-v[p++]=matrix[rowend][col];
-}if(--rowend<rowstart){break;}
-
-
-for(int row=rowend;row>=rowstart;row--){
-v[p++]=matrix[row][colstart];
-}if(colstart++ >colend){break;}
-}return v;
+   int m = matrix.size(), n = m ? matrix[0].size() : 0;
+        int u = 0, d = m - 1;
+        int l = 0, r = n - 1, p = 0;
+        vector<int> order(m * n);
+        while (u <= d && l <= r) {
+            for (int col = l; col <= r; col++) {
+                order[p++] = matrix[u][col];
+            }
+            if (++u > d) {
+                break;
+            }
+            for (int row = u; row <= d; row++) {
+                order[p++] = matrix[row][r];
+            }
+            if (--r < l) {
+                break;
+            }
+            for (int col = r; col >= l; col--) {
+                order[p++] = matrix[d][col];
+            }
+            if (--d < u) {
+                break;
+            }
+            for (int row = d; row >= u; row--) {
+                order[p++] = matrix[row][l];
+            }
+            if (l++ > r) {
+                break;
+            }
+        }
+        return order;
     }
 };
