dfd4b608706a160040152f2a6cd28d442135de75
Wei wei <83396897+Wadiya003@users.noreply.github.com>
2022-02-17 23:44:32 +0530

    Time: 13 ms (45.95%), Space: 10.2 MB (35.19%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,15 +1,21 @@
 class Solution {
-public: //minheap : Apply minheap to k elements and keeps on replacing th etop elements with the latter elements
+public: //minheap : Apply minheap to k elements and keeps on replacing thetop elements with the latter elements //NlogN
     int findKthLargest(vector<int>& nums, int k) {
        priority_queue<int, vector<int>, greater<int>>Q;
-        for(int i: nums){
-            if(Q.size() <k)
-                Q.push(i);
-            else if(Q.top()<i){
-                Q.pop();
-                Q.push(i);
-            }
-        }
-        return Q.top();   
+        // for(int i: nums){
+        //     if(Q.size() <k)
+        //         Q.push(i);
+        //     else if(Q.top()<i){
+        //         Q.pop();
+        //         Q.push(i);
+        //     }
+        // }
+        // return Q.top();   
+        for(int i:nums){
+            Q.push(i);
+            if(Q.size()>k){
+             Q.pop();}
+            
+           }return Q.top();
     }
 };
