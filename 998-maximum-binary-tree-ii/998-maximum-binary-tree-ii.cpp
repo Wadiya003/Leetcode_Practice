251d87361ede7dad9c040fe329e9dd2d5ad6557b
Wei wei <83396897+Wadiya003@users.noreply.github.com>
2022-02-27 00:23:23 +0530

    Time: 4 ms (84.66%), Space: 13.6 MB (30.67%) - LeetHub

---
+++ 998-maximum-binary-tree-ii/998-maximum-binary-tree-ii.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
+       if(!root){
+        return new TreeNode(val);}
+        if(root->val<val){
+         TreeNode* t= new TreeNode(val);
+         t->left=root;
+        root=t;}
+        else{
+        root->right=insertIntoMaxTree(root->right,val);
+            }
+        return root;
+    }
+};
