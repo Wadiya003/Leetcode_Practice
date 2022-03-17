9dc9c80253e57b25b12624d24fa562139441fcf1
Wei wei <83396897+Wadiya003@users.noreply.github.com>
2022-03-17 16:27:33 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (33.90%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public: 
+ //      clockwise rotate
+ // first reverse up to down, then swap the symmetry 
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin(),matrix.end());
+     for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+    }
+};
