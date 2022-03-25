c5d54803f47e48f9aa29f526b693f05e9ea1b971
Wei wei <83396897+Wadiya003@users.noreply.github.com>
2022-03-26 00:32:02 +0530

    Time: 15 ms (14.01%), Space: 12.3 MB (7.87%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,15 +1,30 @@
 class Solution {
-public://can be done recursively //better is greedy 
+public://recursive
+    int solve(int i, int x,int y,vector<vector<int>>& costs,int dp[101][101][101]){
+        if(i>=costs.size()){
+            return 0;
+        }
+        int a=INT_MAX,b=INT_MAX;
+        if(dp[i][x][y]!=-1){
+            return dp[i][x][y];
+        }
+        if(x){
+            a=costs[i][0]+solve(i+1,x-1,y,costs,dp);
+       
+        }
+        if(y){
+            b=costs[i][1]+solve(i+1,x,y-1,costs,dp);
+         
+        }
+        return dp[i][x][y]=min(a,b);
+    }
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        int ans=0;
-        vector<int>diff;
-        for(int i=0;i<costs.size();i++){
-         ans+=costs[i][0];
-         diff.push_back(costs[i][1]-costs[i][0]);
-      }
-        sort(diff.begin(),diff.end());
-        for(int i=0;i<diff.size()/2;i++){
-        ans+=diff[i];
-        }return ans;
+        int n=costs.size();
+        int x=n/2;
+        int y=n/2;
+        int dp[101][101][101];
+        memset(dp,-1,sizeof(dp));
+        return solve(0,x,y,costs,dp);
+        
     }
 };
