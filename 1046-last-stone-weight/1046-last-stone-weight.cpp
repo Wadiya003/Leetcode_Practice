9b3cf8c2e13b8cf9dacc6faf3579b1ce0d710604
Wei wei <83396897+Wadiya003@users.noreply.github.com>
2022-04-07 16:16:21 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (34.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,18 +1,20 @@
 class Solution {
-public:
+public: //priority queue
     int lastStoneWeight(vector<int>& stones) {
-        sort(stones.begin(),stones.end());
-        while(stones.size()>1){
-            int i=stones.size()-1;
-            int first=stones[i],second=stones[i-1];
-            stones.pop_back();
-            stones.pop_back();
-            if(first!=second)
-            stones.push_back(first-second);
-            sort(stones.begin(),stones.end());
+        priority_queue<int>pq;
+         for(auto a:stones){
+          pq.push(a);
+         }
+    while(pq.size()>1){
+    
+      int f= pq.top();pq.pop();
+      int s= pq.top();pq.pop();
+        if(f!=s){
+            pq.push(f-s);
         }
-        if(stones.size()==0)
-            return 0;
-        return stones[0];
+        
+    }
+        if(pq.size()==0)return 0;
+        return pq.top();
     }
 };
