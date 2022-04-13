9dc229535eee04e9ffd9d8d7c8c1aa43c9cd5354
Wei wei <83396897+Wadiya003@users.noreply.github.com>
2022-04-14 00:19:56 +0530

    Time: 131 ms (74.02%), Space: 61.2 MB (61.64%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,14 +1,28 @@
 class Solution {
-public:
+public://Eg :4=> less than 4 would be 4
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int,int>m;
-        int ans=0;
-        for(auto a:nums){
-           m[a]++;}
-        for(auto a:m){
-            if(a.second>1){
-            ans=a.first;
-            break;}
-        }return ans;
+        int n =nums.size();
+        int l=1;
+        int r=nums.size()-1;
+        while(l<=r){
+       int m=l+(r-l)/2;
+       int cnt = 0;
+            //search for smaller numbers than mid
+            for(auto num : nums) if(num <= m) cnt++;
+            //if smaller numbers cnt is less than or equal to mid ,that means duplicate is in right side ,therfore discard left search space
+            if(cnt <= m) l = m+1;
+            //else verdict is in left search space
+            else r = m-1;
+       
+        }
+        return l;
+        
     }
+    // bool isokay(vector<int>&nums,int m){
+    //     int tot=0;
+    //     for(int i=0;i<nums.size();i++){
+    //      if(nums[i]<=m)tot++;
+    //     }
+    //     return tot==m;
+    // }
 };
