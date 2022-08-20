cb4cd1687194598ba06d8ffd81065cf0e292ec45
Wei wei <83396897+Wadiya003@users.noreply.github.com>
2022-08-20 13:00:33 +0530

    Time: 558 ms (5.00%), Space: 109.1 MB (5.00%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
-       
-        vector<long>dp(stations.size()+1,0);
-        dp[0]=startFuel;//no station = can rech till start Fuel
-        for(int i=0;i<stations.size();i++){//add stations
-           for(int j=i;j>=0;--j){
-               if(dp[j]>=stations[i][0]) //can reach next station
-                   dp[j+1]=max(dp[j+1],dp[j]+ (long)stations[i][1]);
-               //take mx distance we can reach
-           }
+        int n =stations.size();
+         //dp: From first i fuels, we used j to get max dist
+        vector<vector<long>>dp(n+1,vector<long>(n+1,0));
+         //don't fuel at any station-> Farthest==startFuel
+        for(int i=0;i<n+1;i++)
+        dp[i][0]=startFuel;
+        
+        for(int i=1;i<n+1;i++){
+            for(int j =1;j<n+1;j++){
+                //didn't fuel up==equal to last stations val
+                dp[i][j]=dp[i-1][j];
+                 //refuel at current
+            if(dp[i-1][j-1]>=stations[i-1][0]){
+                //if can reach this station from pre
+               
+                dp[i][j]=max(dp[i-1][j],dp[i-1][j-1]+stations[i-1][1]);//take max dist
+                }
+            }
         }
-        for(int i=0;i<=stations.size();++i){
-            if(dp[i]>=target){
+        
+        for(int i=0;i<n+1;i++){
+            if(dp[n][i]>=target){
                 return i;
             }
         }
