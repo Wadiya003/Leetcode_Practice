d9ebb719912911c1076c24e896b9756ef373bf36
Wei wei <83396897+Wadiya003@users.noreply.github.com>
2022-09-13 23:33:42 +0530

    Time: 16 ms (72.82%), Space: 14.1 MB (38.65%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int count = 0;
+        for (auto d : data) {
+            if (count == 0) {
+                if ((d >> 5) == 0b110) 
+                    count = 1;
+                else if ((d >> 4) == 0b1110) 
+                    count = 2;
+                else if ((d >> 3) == 0b11110) 
+                    count = 3;
+                else if ((d >> 7)) 
+                    return false;
+            } else {
+                if ((d >> 6) != 0b10) 
+                    return false;
+                    count--;
+            }
+        }
+        return count == 0;
+    }
+};
