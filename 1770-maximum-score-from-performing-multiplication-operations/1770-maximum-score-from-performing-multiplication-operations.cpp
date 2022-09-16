fddc3d0dc12c2d658a9efa2d37a597ea288c6e2a
Wei wei <83396897+Wadiya003@users.noreply.github.com>
2022-09-16 11:55:16 +0530

    Time: 539 ms (74.90%), Space: 50.2 MB (99.87%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -3,14 +3,17 @@ public:
     //iterative dp
    
     int maximumScore(vector<int>& nums, vector<int>& multipliers) {
-        int dp[1001][1001];
+        int dp[1001];
         memset(dp,0,sizeof(dp));
         for(int i=multipliers.size()-1;i>=0;i--){
-            for(int start=i;start>=0;start--){
-               dp[i][start]= max(multipliers[i]*nums[start]+dp[i+1][start+1],multipliers[i]*nums[nums.size()-(i-start)-1]+dp[i+1][start]);
+            for(int start=0;start<=i;start++){
+               int end=nums.size()-1-(i-start);
+               int l=multipliers[i]*nums[start]+dp[start+1];
+               int r=multipliers[i]*nums[end]+dp[start];
+               dp[start]=max(l,r);
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
     
 };
